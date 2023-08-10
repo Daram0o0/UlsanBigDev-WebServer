5055a4f18b87fb32ed5c9a08140d8685cc89eed5
Daram0o0 <c30728535@gmail.com>
2023-08-11 00:55:13 +0900

    login 기능 연동 중

--- index.js
+++ index.js
@@ -1,108 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const cors = require('cors')
-// const fs = require('fs')
-// const bodyParser = require('body-parser');
-// const filePath = 'data.json';
-
-// app.use(cors())
-// app.use(express.json())
-// app.use(bodyParser.json());
-
-// const portfolio = [
-// ]
-
-// app.get('/', (req, res) => {
-//   res.send("home");
-// })
-
-// app.get('/portfolio', (req, res) => {
-//   fs.readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send({ error: 'An error occurred while reading the data file.' });
-//       return;
-//     }
-
-//     try {
-//       const parsedData = JSON.parse(data);
-//       res.json(parsedData);
-//     } catch (parseError) {
-//       console.error(parseError);
-//       res.status(500).send({ error: 'An error occurred while parsing the data.' });
-//     }
-//   });
-// });
-
-
-// app.post('/portfolioInsert', (req, res) => {
-//   // const reqName = req.body.name
-//   // const reqPortfolioForm = req.body.portfolioForm
-//   const reqName = req.body.name
-//   const reqPort = req.body.portfolioForm
-//   const reqTitle = req.body.title
-//   const reqContent = req.body.content
-//   const reqStartDate = req.body.startDate
-//   const reqEndDate = req.body.endDate
-//   const reqHeadCount = req.body.headCount
-//   const reqList = req.body.list
-
-//   for (let i = 0; i < portfolio.length; i++) {
-//     if (portfolio[i].name === reqName) {
-//       portfolio[i].portfolioForm.push({
-//         title: reqTitle,
-//         content: reqContent,
-//         startDate: reqStartDate,
-//         endDate: reqEndDate,
-//         headCount: reqHeadCount,
-//         list: reqList
-//       })
-//       res.send({ msg: "완료" })
-//     }
-//   }
-
-//   const jsonFile = JSON.stringify(portfolio);
-//   fs.writeFile(filePath, jsonFile, 'utf8', (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.send({ msg: err });
-//     } else {
-//       console.log("success");
-//       res.send({ message: "success" });
-//     }
-//   })
-
-//   // res.send(reqTitle)
-//   // console.log(reqTitle)
-//   // console.log(reqList)
-//   // console.log("이름", reqName, "/포폴", reqPort)
-
-// })
-
-// app.get('/portfolio/addMember', (req, res) => {
-//   const reqName = req.query.name
-//   portfolio.push({
-//     name: reqName,
-//     portfolioForm: []
-//   })
-//   res.send({ msg: "완료" })
-//   // const jsonFile = JSON.stringify(portfolio);
-
-//   // fs.writeFile(filePath, jsonFile, 'utf8', (err) => {
-//   //   if (err) {
-//   //     console.log(err);
-//   //     res.send({ msg: err });
-//   //   } else {
-//   //     console.log("success");
-//   //     res.send({ message: "success" });
-//   //   }
-//   // })
-// })
-
-// app.listen(5050, () => { console.log("5050 port open") })
-
-
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -113,6 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 let portfolio = [];
+
+// const admin =
+// {
+//   userId: "admin",
+//   userPw: "1234",
+// }
 
 // 서버 시작 시 데이터 파일에서 읽어와서 portfolio 배열에 로드
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -130,6 +31,10 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 app.get('/', (req, res) => {
   res.send("home");
 });
+
+// app.get('/admin', (req, res) => {
+//   res.send(admin)
+// })
 
 app.get('/portfolio', (req, res) => {
   res.json(portfolio);
